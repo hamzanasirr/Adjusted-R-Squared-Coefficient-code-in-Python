877599dca338d819002d0d3b23888a1a436cadf2
Hamza <hamzanasirr@gmail.com>
2019-09-11 22:23:44 +0500

    Code documented.

--- adjusted_r_squared.py
+++ adjusted_r_squared.py
@@ -1,4 +1,18 @@
 def adjusted_r_squared(X, Y, r2):
+    '''
+    Returns a computed Adjusted R-Squared Coefficient.
+    
+    Parameters
+    ----------
+    X : Pandas DataFrame
+        A pandas DataFrame including all the independant variables. Could be a series if there is only one predictor.
+        
+    Y : Pandas DataFrame or Series
+        Labels or response variables 'Y'.
+        
+    r2 : float
+        R-Squared Coefficient
+    '''
     n = len(Y)
     p = X.shape[1]
     adj_r = 1 - ((1 - r2) * (n - 1)) / (n - p - 1)
